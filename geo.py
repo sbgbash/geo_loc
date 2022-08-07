a06ec15eba1ec49232a9aa10727c4600c4010b39
Sean Giblin <sbgbash@gmail.com>
2022-08-07 12:43:13 -0700

    breaking into functions

--- geo.py
+++ geo.py
@@ -3,7 +3,6 @@ import overpy
 import geopy.distance
 import geocoder
 
-
 def main():
     # get current location of computer
     loc = get_location()
@@ -12,32 +11,38 @@ def main():
   
     # establish the API
     api = overpy.Overpass()
+    data = query_OSM(loc_str, loc, api)
+    print_data(data)
 
-    # use API to query openStreetMaps 
+# query the OSM databse for relevant info
+def query_OSM(loc_str, loc, api,):
     try:
         result = api.query(f"""[out:json][timeout:25];(nwr["building"="yes"]
             (around: 10000, {loc_str}););
             out body;>;out skel qt;""")
     except overpy.exception.OverpassGatewayTimeout:
         print("OSM Server load too high - please try again later")
-   
-    # create dict to store builduing name and distance/location
-    data = {}
-    
-    # iterate through way objects if there is a building grab name/coordinates
-    for way in result.ways:
-        if way.tags.get('name', 'n/a') != 'n/a':
-            lat_lon = (f'{round(way.nodes[0].lat, 5)}, {round(way.nodes[0].lon, 5)}')
-            data.update({way.tags.get('name', 'n/a'): [round(geopy.distance.geodesic( \
+    else:
+        # create dict to store builduing name and distance/location
+        d_dict = {}
+        # iterate through way objects if there is a building grab name/coordinates
+        for way in result.ways:
+            if way.tags.get('name', 'n/a') != 'n/a':
+                lat_lon = (f'{round(way.nodes[0].lat, 5)}, {round(way.nodes[0].lon, 5)}')
+                d_dict.update({way.tags.get('name', 'n/a'): [round(geopy.distance.geodesic( \
                 loc, lat_lon).miles, 2), lat_lon]})
+        return d_dict
 
-    # print out closest building
+
+# print out closest building
+def print_data(data):
     for k, v in data.items():
         print(f'Building: {k}, distance: {v[0]} miles, loc: {v[1]}')
+
 
 # function to get current computer location using IP
 def get_location():
     g = geocoder.ip('me')
-    return g.latlng
+    return (g.latlng)
 
 main()
